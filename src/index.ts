880e30d795b348bfcdbd0794a39eb5b33eae543c
Bruno Lopes <eusoubruno7@gmail.com>
2023-07-21 18:49:25 -0300

    feat: Create models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
 
-app.listen(3333, () => {
-  console.log('server is runnig on port 3333');
-});
+    app.listen(3333, () => {
+      console.log('server is runnig on port 3333!');
+    });
+
+  })
+  .catch((error) => {
+    console.log('DB connection falied!', error);
+  });
